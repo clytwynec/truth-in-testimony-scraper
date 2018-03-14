527d60474d9743f35ef5bbcffa064118d03e8ec4
Christine Lytwynec <chris.lytwynec@gmail.com>
2018-03-14 16:14:53 -0400

    getting selenium crawl working and pushing files to s3

---
+++ boto_util.py
@@ -0,0 +1,15 @@
+import boto3
+
+def upload_to_s3(file_data, file_path, bucket_name):
+    # Create an S3 client
+    s3 = boto3.client('s3')
+    res = s3.put_object(
+        Bucket=bucket_name,
+        Key=file_path,
+        Body=file_data,
+        ACL='public-read',
+        ContentType='application/pdf',
+    )
+    return '{}/{}/{}'.format(s3.meta.endpoint_url, bucket_name, file_path)
+
+
--- crawl_ttf.py
+++ crawl_ttf.py
@@ -1,63 +1,96 @@
 # -*- coding: utf-8 -*-
 import os
+import sys
 
-import boto3
+import requests
 from selenium import webdriver
 from termcolor import colored
 
+from boto_util import upload_to_s3
 
 start_url = 'http://docs.house.gov/Committee/Search/Home.aspx?Keyword=truth+in+testimony'
 next_xpath = '//*[@id="MainContent_btnNext"]'
+pages_crawled = 1
+ttfs_found = 0
 
 
-def upload_to_s3(file_data, file_name, bucket_name):
-    print file_name, bucket_name
-    # Create an S3 client
-    s3 = boto3.resource('s3')
-    s3Obj = s3.Object(bucket_name, file_name)
-
-    s3Obj.put(
-        ACL='public-read',
-        Body=file_data,
-        ContentType='application/pdf',
-    )
-
-
-def is_ttf(url):
+def looks_like_ttf(url):
     """
     Args:
         url: String 
     Returns: Bool
     """
-    fn = url("/")[-1].lower
+    fn = url.split("/")[-1].lower()
     return ('-ttf-' in fn) or ('disclosure' in fn) or ('truthintest' in fn) or ('-wstate-' in fn and 'sd0' in fn)
 
 
-def save_ttfs_from_page(response):
-    if is_ttf(file_name) and response.headers['Content-Type']=='application/pdf':
-        split_url = response.url.split("/")
-        # page = split_url[-1]
-        domain = split_url[2]
-        # committee_code = split_url[4].upper()
+def save_ttfs_from_page(driver):
+    global ttfs_found
 
-        print colored('********* Uploading to S3: ' + file_name, 'cyan')
-        # upload_to_s3(response.body, committee_code + '/' + file_name, 'ttf-forms')
-        upload_to_s3(response.body, '03142018' + '/' + file_name, 'ttf-forms')
+    elems = driver.find_elements_by_xpath("//a[@href]")
+    for elem in elems:
+        href = elem.get_attribute("href")
+        if looks_like_ttf(href):
+            # Fetch the content at that URL
+            r = requests.get(href, stream=True)
+
+            # Check that content-type is a PDF
+            if not r.headers['Content-Type']=='application/pdf':
+                # If it's not, move on to the next link
+                continue
+
+            meeting_elem = elem.find_element_by_xpath('../ancestor::li[@class="search"]')
+            info = {'orig_form_url': r.url}
+            info['meeting_title'] = meeting_elem.find_element_by_xpath('./h3/a').text
+            info['meeting_id'] = meeting_elem.find_element_by_xpath('./h3/small').text
+            info['meeting_status'] =  meeting_elem.find_element_by_xpath('./ul/small').text
+            info['meeting_details'] = meeting_elem.find_element_by_xpath('./ul').text.split('\n')[0]
+
+            ttfs_found += 1
+            file_name = r.url.split("/")[-1]
+            
+            if keep_data:
+                # Upload TTF to S3
+                print colored('********* Uploading to S3: ' + file_name, 'cyan')        
+                info['s3_form_url'] = upload_to_s3(r.content, '03142018' + '/' + file_name, 'ttf-forms')
+                
+                # Store metadata in google sheets
+                print colored('********* Saving metadata for: ' + file_name, 'cyan')        
+                # TODO
+            else:
+                print colored('********* Would upload to S3: ' + file_name, 'yellow')        
+                print colored('********* Would save metadata for: ' + file_name, 'yellow')        
+
+
+def print_crawl_summary():
+    print 'Done crawling.\n'
+    print '____CRAWL SUMMARY____'
+    print str(pages_crawled) + ' pages crawled'
+    print str(ttfs_found) + ' TTFs found\n'
 
 
 def crawl_it():
+    global pages_crawled
+
     driver = webdriver.Firefox()
     driver.get(start_url)
 
     while True:
-        next_btn = driver.find_element_by_xpath(next_xpath)
+        # Save TTFs on current page
+        save_ttfs_from_page(driver)
+        if short_run and pages_crawled == 3:
+            break
         try:
-            import ipdb; ipdb.set_trace()
+            next_btn = driver.find_element_by_xpath(next_xpath)
             next_btn.click()
+            print colored('Clicked next button, count ' + str(pages_crawled), 'white', 'on_cyan')
+            pages_crawled += 1
         except:
             break
 
+    print_crawl_summary()
     driver.close()
 
-
+keep_data = '-d' not in sys.argv[1:]
+short_run = '-q' in sys.argv[1:]
 crawl_it()
